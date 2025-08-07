608f6a8ad75dfc8a4396a664e0808ca5f3bb04c8
SayedThoha <147703696+SayedThoha@users.noreply.github.com>
2025-08-07 12:57:11 +0530

    small chandge in print values

--- linkedList.js
+++ linkedList.js
@@ -19,6 +19,7 @@ class LinkedList {
     return this.size === 0;
   }
 
+  // add new node to the beginning
   prepend(value) {
     const newNode = new Node(value);
 
@@ -31,6 +32,8 @@ class LinkedList {
 
     this.size++;
   }
+
+  // add new node to end of the list
   append(value) {
     const newNode = new Node(value);
     if (this.isEmpty()) {
@@ -45,4 +48,23 @@ class LinkedList {
     }
     this.size++;
   }
+
+  printList() {
+    let listValues = "";
+    let curr = this.head;
+    while (curr) {
+      listValues += `${curr.value}-> `;
+      curr = curr.next;
+    }
+    console.log(listValues);
+  }
 }
+
+const ll = new LinkedList();
+
+ll.append(1);
+ll.append(2);
+ll.append(3);
+ll.prepend(22);
+ll.prepend(23);
+ll.printList();
